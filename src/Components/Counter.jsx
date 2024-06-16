4f1e4569f967cf6a621362302fe91a94381fe374
TejaswiniKakarlamudi <kakarlamuditejaswini@gmail.com>
2024-06-16 14:03:35 +0530

    Counter App

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,24 +1,32 @@
 import React, { useState } from 'react'
-
+import styles from './Counter.module.css';
 function Counter() {
     const [count, setCount] = useState(0);
-
+    const [extra, setExtra] = useState(1);
     function handleIncrement(){
-        setCount(count+1);
+        setCount(count+extra);
         console.log('Increment');
     };
     function handleDecrement(e){
-        setCount(count-1);
+        setCount(count-extra);
         console.log('Decrement');
     };
+    function handleOnChange(e){
 
+        setExtra(Number(e));
+    };
   return (
-    <div>
-      <h1>Counter App</h1>
-
-      <h4>Count: {count}</h4>
-      <button onClick={handleIncrement}>Increment</button>
-      <button onClick={handleDecrement}>Decrement</button>
+    <div className={styles.set}>
+      <div className={styles.counter}>
+        <h1 className={styles.heading}>Counter App</h1>
+        <h4 className={styles.count}>Count: {count}</h4>
+        <input type="number" name="extra" value={extra} onChange={(e)=>handleOnChange(e.target.value)} className={styles.extra}/>
+        <div>
+            <button onClick={handleIncrement} className={styles.increment}>Increment</button>
+            <button onClick={handleDecrement} className={styles.decrement}>Decrement</button>
+        </div>
+        
+      </div>
     </div>
   )
 }
